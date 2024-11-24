62f5a296dd22ddc4c9eef18b68f6077dea3894a7
gmshimon <simon.rosedale99@gmail.com>
2024-11-25 05:01:31 +0600

    application details show

--- Frontend/src/Pages/Dashboard/MyApplication/MyApplication.jsx
+++ Frontend/src/Pages/Dashboard/MyApplication/MyApplication.jsx
@@ -1,47 +1,65 @@
-import { FcViewDetails } from "react-icons/fc"
+import { FcViewDetails } from 'react-icons/fc'
+import ApplicationStatus from '../../../Component/Dashboard/ApplicationStatus/ApplicationStatus'
 
 const MyApplication = () => {
   return (
-    <section>
+    <section className='h-[calc(100vh-29px)]'>
       <h1 className='text-4xl m-7'>My Applications</h1>
-      <div className='bg-white mx-7 mt-10 px-10 rounded-xl'>
-        <div className='overflow-x-auto'>
-          <table className='table rounded-lg'>
+      <div className='bg-white mx-7 mt-10 px-10 py-10 rounded-xl'>
+        <div className='overflow-x-auto '>
+          <table className='table '>
             {/* head */}
-            <thead className='bg-orange-600 text-black'>
+            <thead className=' bg-blue-400 text-black text-lg'>
               <tr>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th></th>
+                <th>Title</th>
+                <th className=''>Job</th>
+                <th>Date</th>
+                <th>Details</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <td>
+              <tr className='text-[17px]'>
+                <td className='w-[350px]'>
                   <div className='flex items-center gap-3'>
                     <div>
-                      <div className='font-bold'>Hart Hagerty</div>
-                      <div className='text-sm opacity-50'>United States</div>
+                      <div className='font-bold'>Product Designer</div>
+                      <div className='badge badge-accent mt-2 badge-sm p-2'>
+                        Accepted
+                      </div>
                     </div>
                   </div>
                 </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className='badge badge-ghost badge-sm'>
-                    Desktop Support Technician
-                  </span>
+                <td className='w-[350px]'>
+                  <p className='text-red-600 '>Full Time</p>
+                  <p>Yearly Salary, Germany</p>
                 </td>
-                <td>Purple</td>
+                <td className='w-[300px]'>20-11-2024</td>
                 <th>
-                  <button className='btn btn-ghost btn-xs text-2xl'><FcViewDetails/></button>
+                  <button
+                    className='btn btn-ghost btn-xs text-2xl'
+                    title='Details'
+                    onClick={() =>
+                      document.getElementById('my_modal_5').showModal()
+                    }
+                  >
+                    <FcViewDetails />
+                  </button>
                 </th>
               </tr>
-
             </tbody>
           </table>
+          <dialog id='my_modal_5' className='modal'>
+            <div className='modal-box'>
+              <form method='dialog'>
+                {/* if there is a button in form, it will close the modal */}
+                <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+                  ✕
+                </button>
+              </form>
+              <ApplicationStatus />
+            </div>
+          </dialog>
         </div>
       </div>
     </section>
